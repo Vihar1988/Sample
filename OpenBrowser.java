1d0033d6bfb13d53faa8a32632aba7612d1f892b
Vihar1988 <53364063+Vihar1988@users.noreply.github.com>
2019-09-03 13:16:08 +0530

    Newly changed

--- OpenBrowser.java
+++ OpenBrowser.java
@@ -8,12 +8,24 @@ public class OpenBrowser {
 
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "D:\\Prasanna\\Backup\\chromedriver_win32\\chromedriver.exe");
-WebDriver driver = new ChromeDriver();
-driver.get("http://automationpractice.com/index.php");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://newtours.demoaut.com/mercuryreservation.php");
+		driver.manage().window().maximize();
+		driver.findElement(By.name("userName")).sendKeys("demo");
+		driver.findElement(By.name("password")).sendKeys("demo");
+		driver.findElement(By.name("login")).click();
+		WebElement radio1 = driver.findElement(By.xpath("/html/body/div/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[2]/td[2]/b/font/input[2]"));
+		radio1.click();
 
-driver.findElement(By.cssSelector("login")).click();
+		WebElement drop=driver.findElement(By.name("passCount"));
+		Select select=new Select(drop);
+select.selectByIndex(0);
 
-		
+List<WebElement> options= select.getOptions();
+int size=options.size();
+System.out.println("No of passenger" + size);
+
 	}
+
 
 }
